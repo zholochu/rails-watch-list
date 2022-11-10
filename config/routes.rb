5e99bfdac4f100e888a6a1cf7e079315b1029cc2
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2022-11-10 16:51:17 -0500

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get '/bookmarks/new', to: 'bookmarks#new'
+  # get '/bookmarks/new', to: 'bookmarks#new'
 
   # # LISTS
   # get '/lists', to: 'lists#index'
   # get '/lists/:id', to: 'lists#show'
   # get '/lists/new', to: 'lists#new'
-  resources :lists, only: [:index, :show, :new, :create]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
+  resources :bookmarks, only: [:destroy]
+
 end
