5aa6d64b768b70a36e29d69cfd4cba1ec5938f8f
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2022-11-11 16:54:56 -0500

    Code update

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # get '/lists', to: 'lists#index'
   # get '/lists/:id', to: 'lists#show'
   # get '/lists/new', to: 'lists#new'
-
+  root to: 'lists#index'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
